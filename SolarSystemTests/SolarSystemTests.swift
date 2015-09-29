082000cac06e64cf85f61eef0ff9f5c9ab96e389
Caleb Hicks <calebhicks@gmail.com>
2015-09-29 15:09:38 -0600

    Initial Commit

---
+++ SolarSystemTests/SolarSystemTests.swift
@@ -0,0 +1,36 @@
+//
+//  SolarSystemTests.swift
+//  SolarSystemTests
+//
+//  Created by Caleb Hicks on 9/29/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import XCTest
+@testable import SolarSystem
+
+class SolarSystemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
