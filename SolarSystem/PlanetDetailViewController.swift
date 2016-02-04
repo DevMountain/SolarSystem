3be0e6c583d82d04dfa1f31f13bec23449d29a27
Youcef Bouhafna <bouhafnayoucef@gmail.com>
2016-02-03 19:04:51 -0700

    Solar System app

---
+++ SolarSystem/PlanetDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlanetDetailViewController.swift
+//  SolarSystem
+//
+//  Created by youcef bouhafna on 2/3/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class PlanetDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
